f1f61779609bd5aac2e68f9d3d68da067b62b4f2
BENTOTV <agusjuniartha16@gmail.com>
2023-02-17 22:51:10 +0800

    traning go

---
+++ day1/day1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for int i =1; i<=5;i++{
+
+	fmt.Println(len("hello world"))
+	fmt.Println("kamu"[0])
+}
---
+++ day2/varialbes.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "agus juniartha"
+	fmt.Println(name)
+
+	name = "bentotv"
+	fmt.Println(name)
+
+	var fiend = "budi"
+	fmt.Println(fiend)
+
+	age := 34
+	fmt.Println(age)
+
+	var aku string
+	aku = "saya"
+	fmt.Println(aku)
+	var (
+		namaDepan    = "agus"
+		namaBelakang = "juniartha"
+	)
+	fmt.Println(namaDepan + " " + namaBelakang)
+
+}
